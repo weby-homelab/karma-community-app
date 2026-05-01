49542dc75ad3985294b635909b0dac1b7b72c045
weby-homelab <rekvizitor.ua@gmail.com>
2026-05-01 17:22:00 +0300

    fix(security): Correctly parse TRUST_PROXY env var type

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-app.set('trust proxy', process.env.TRUST_PROXY || 1);
+app.set('trust proxy', process.env.TRUST_PROXY ? (isNaN(process.env.TRUST_PROXY) ? (process.env.TRUST_PROXY === 'true' ? true : (process.env.TRUST_PROXY === 'false' ? false : process.env.TRUST_PROXY)) : Number(process.env.TRUST_PROXY)) : 1);
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
